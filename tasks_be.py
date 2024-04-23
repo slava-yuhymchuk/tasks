7cee03851208dc64d64c0eda739b4e5d34245b5c
Slava Yuhymchuk <slava.yuhymchuk@gmail.com>
2024-04-23 14:07:08 +0300

    implemented add_task function

--- tasks_be.py
+++ tasks_be.py
@@ -5,8 +5,9 @@ file_name = "tasks.json"
 
 def create_file():
     if not os.path.isfile(file_name):
+        json_data = {"Tasks": []}
         json_file = open(file_name, "w")
-        json.dump({"tasks": []}, json_file)
+        json.dump(json_data, json_file, indent=4)
         json_file.close()
 
 def read_file():
@@ -16,18 +17,12 @@ def read_file():
     json_file.close()
     return json_data
 
-def get_tasks():
-    tasks = read_file().get("tasks",[])
-    return tasks
-
-def get_task(id):
-    task = next((task for task in get_tasks() if task["id"] == id), None)
-    return task
-    #return jsonify(task.to_json())
-
-def add_task(task_id,task_name,task_details):
-    pass
-
+def write_file(tasks):
+    json_data = {"Tasks": tasks}
+    json_file = open(file_name, "w")
+    json.dump(json_data, json_file, indent=4)
+    json_file.close() 
+    
 def delete_task(task_id):
     pass
 
@@ -35,7 +30,7 @@ def update_task(task_id,task_name,task_details):
     pass
 
 if __name__ == "__main__":
-    a = get_tasks()
+    a = read_file()
     print (a)
 
 #a = tasks.jsonify('"1": {"title":"buy","details":"milk"}')
--- tasks_fe.py
+++ tasks_fe.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
 import json
 import tasks_be
 
@@ -7,25 +6,36 @@ app = Flask(__name__)
 
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
-    return tasks_be.get_tasks()
+    tasks = tasks_be.read_file().get("Tasks",[])
+    return tasks
 
 @app.route("/tasks/<int:id>", methods=["GET"])
 def get_task(id):
-    return tasks_be.get_task(id)
+    task = next((task for task in get_tasks() if task["ID"] == id), None)
+    if task: return task
+    else: abort(404)
 
 @app.route("/tasks", methods=["POST"])
 def add_task():
-    new_task = request.data
-    tasks_be.add_task(new_task)
-    return json.dumps(new_task)
+    tasks = tasks_be.read_file().get("Tasks",[])
+    id = len(tasks)+1
+    title = request.json["Title"]
+    details = request.json["Details"]
+    task = {"ID": id, "Title": title, "Details": details, "Status": "New"}
+    tasks.append(task)
+    try: tasks_be.write_file(tasks)
+    except Exception as error: abort(500, error)
+    finally: return task
 
 @app.route("/tasks/<int:task_id>", methods=["POST"])
-def delete_task(task_id):
+def delete_task(id):
     pass
 
 @app.route("/tasks/<int:task_id>", methods=["POST"])
-def update_task(task_id):
+def update_task(id):
     pass
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port=5000)
+    #a = get_task(4)
+    #print (a)
