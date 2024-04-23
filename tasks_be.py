7bf0e4345e2c4bc858164763c88cbd2c84dda983
Slava Yuhymchuk <slava.yuhymchuk@gmail.com>
2024-04-23 19:11:23 +0300

    implemented delete_task function

--- tasks_be.py
+++ tasks_be.py
@@ -3,14 +3,14 @@ from json import load, dump
 
 file_name = "tasks.json"
 
-# Create new empty JSON file
+# Create new empty JSON file.
 def create_file():
         json_data = {"Tasks": []}
         write_file(json_data)
 
-# Read the contents of the JSON file
+# Read the contents of the JSON file.
 def read_file():
-    # Initialize with and empty JSON file if one still doesn't exist
+    # Initialize with and empty JSON file if one still doesn't exist.
     if not path.isfile(file_name):
         create_file()
     json_file = open(file_name,"r")
@@ -18,13 +18,13 @@ def read_file():
     json_file.close()
     return json_data
 
-# Overwrite the contents of the JSON file
+# Overwrite the contents of the JSON file.
 def write_file(json_data):
     json_file = open(file_name, "w")
     dump(json_data, json_file, indent=4)
     json_file.close()
 
-# Write tasks to the JSON file
+# Write tasks to the JSON file.
 def write_tasks(tasks):
     json_data = {"Tasks": tasks}
     write_file(json_data)
--- tasks_fe.py
+++ tasks_fe.py
@@ -3,23 +3,23 @@ import tasks_be
 
 app = Flask(__name__)
 
-# Return all tasks
+# Return all tasks.
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
     tasks = tasks_be.read_file().get("Tasks",[])
     return tasks
 
-# Return specific task by ID
+# Return specific task by ID.
 @app.route("/tasks/<int:id>", methods=["GET"])
 def get_task(id):
     task = next((task for task in get_tasks() if task["ID"] == id), None)
     if task: return task
     else: abort(404)
 
-# Add new task with POST request data
+# Add new task with POST request data. Return newly created task.
 @app.route("/tasks", methods=["POST"])
 def add_task():
-    tasks = tasks_be.read_file().get("Tasks",[])
+    tasks = get_tasks()
     ids = [0]
     for task in tasks:
         ids.append(task["ID"])
@@ -32,22 +32,25 @@ def add_task():
     except Exception as error: abort(500, error)
     finally: return task
 
-# Delete specific task by ID
-@app.route("/tasks/<int:task_id>", methods=["DELETE"])
+# Delete specific task by ID. Return deleted task.
+@app.route("/tasks/<int:id>", methods=["DELETE"])
 def delete_task(id):
-    pass
+    task = get_task(id)
+    tasks = get_tasks()
+    tasks = [task for task in tasks if task["ID"] != id]
+    try: tasks_be.write_tasks(tasks)
+    except Exception as error: abort(500, error)
+    finally: return task
 
-# Update specific task with PUT request data
-@app.route("/tasks/<int:task_id>", methods=["PUT"])
+# Update specific task with PUT request data. Return updated task.
+@app.route("/tasks/<int:id>", methods=["PUT"])
 def update_task(id, title, details):
     pass
 
-# Change the status of specific task to "Done"
-@app.route("/tasks/<int:task_id>", methods=["PUT"])
+# Change the status of specific task to "Done". Return completed task.
+@app.route("/tasks/<int:id>/done", methods=["PUT"])
 def mark_done(id):
     pass
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    # a = add_task()
-    # print (a)
