6edfe1f7e7f6379cd94a45c0d9321981a8086ff9
Slava Yuhymchuk <slava.yuhymchuk@gmail.com>
2024-07-11 19:10:40 +0300

    added terraform and jenkins folders

---
+++ Terraform/DEV/VPC.tf
@@ -0,0 +1,9 @@
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "il-central-1"
+  #shared_config_files = ["~/.aws/config"]
+  #shared_credentials_files = ["~/.aws/credentials"]
+  profile = "slava"
+}
+
