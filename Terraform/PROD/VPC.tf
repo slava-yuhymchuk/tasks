d773440671f80cc04e832a3957d3c34d65a9c534
Slava Yuhymchuk <slava.yuhymchuk@gmail.com>
2024-07-11 19:38:25 +0300

    added tf files

--- Terraform/PROD/VPC.tf
+++
@@ -1,9 +0,0 @@
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "il-central-1"
-  #shared_config_files = ["~/.aws/config"]
-  #shared_credentials_files = ["~/.aws/credentials"]
-  profile = "slava"
-}
-
