e0542061150c001b476745a4b1a1783728edd60f
Slava Yuhymchuk <slava.yuhymchuk@gmail.com>
2024-07-13 14:32:57 +0300

    added nat gateway

--- Terraform/VPC/vpc.tf
+++ Terraform/VPC/vpc.tf
@@ -16,7 +16,7 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  one_nat_gateway_per_az = false
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.single_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
 }
