30f335fb56d3d2a8c1403611e7dd0703a929570e
Slava Yuhymchuk <slava.yuhymchuk@gmail.com>
2024-10-15 22:12:46 +0300

    added sg for istio

--- Terraform/DEV/eks.tf
+++ Terraform/DEV/eks.tf
@@ -46,3 +46,13 @@ resource "aws_security_group_rule" "sealed-secrets-access" {
   source_security_group_id = module.eks.cluster_security_group_id
   description              = "Allow inbound traffic to sealed-secrets controller"
 }
+
+resource "aws_security_group_rule" "istio" {
+  description              = "Istio"
+  type                     = "ingress"
+  from_port                = 15017
+  to_port                  = 15017
+  protocol                 = "tcp"
+  source_security_group_id = module.eks.cluster_security_group_id
+  security_group_id        = module.eks.node_security_group_id
+}
